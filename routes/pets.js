7a1c7a57161c5698aac87fca393391bdbd32aaea
Jordan_Torres <jt.arontorres@gmail.com>
2021-08-30 22:33:51 -0700

    Implemented search pagination

--- routes/pets.js
+++ routes/pets.js
@@ -58,14 +58,19 @@ module.exports = (app) => {
   // SEARCH PET
   // the i variable means we are doing case-insensitive matching with our searching
   app.get('/search', (req, res) => {
-    term = new RegExp(req.query.term, 'i')
-    // adding the name and species so we can search by either 
-    Pet.find({$or:[
-      {'name': term},
-      {'species': term}
-    ]}).exec((err, pets) => {
-      res.render('pets-index', { pets: pets });
-    })
+    const term = new RegExp(req.query.term, 'i')
+
+    const page = req.query.page || 1
+    Pet.paginate(
+      {
+        $or: [
+          { 'name': term },
+          { 'species': term }
+        ]
+      },
+      { page: page }).then((results) => {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term });
+      });
   });
 }
   
