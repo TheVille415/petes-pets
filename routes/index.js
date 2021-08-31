28dc8176fd17bf5a42446a5a2af22d9983f44276
Jordan_Torres <jt.arontorres@gmail.com>
2021-08-30 22:25:54 -0700

    Implemented pagination on the home page

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,13 @@ module.exports = (app) => {
   /* GET home page. */
   // changing this to a pagination version
   app.get('/', (req, res) => {
-    Pet.paginate().then((results) => {
-      res.render('pets-index', { pets: results.docs });    
+    const page = req.query.page || 1
+  
+    Pet.paginate({}, {page: page}).then((results) => {
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });    
     });
   });
+  
 }
 // with this we are now limiting how many results we are getting on each page
+// with this next version we are able to access each page with 3 results per page
