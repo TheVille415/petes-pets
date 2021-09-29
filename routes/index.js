aea1560bb43954f452ea0382bf79dcf166c2ae16
Jordan_Torres <jt.arontorres@gmail.com>
2021-09-28 22:29:44 -0700

    Implemented responding to JSON

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,19 @@ const Pet = require('../models/pet');
 
 module.exports = (app) => {
 
-  /* GET home page. */
-  // changing this to a pagination version
   app.get('/', (req, res) => {
     const page = req.query.page || 1
-  
-    Pet.paginate({}, {page: page}).then((results) => {
-      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });    
+
+    Pet.paginate({}, { page: page }).then((results) => {
+      // If the request is JSON, we want to send a JSON response
+      if (req.header('Content-Type') == 'application/json') {
+        return res.json({ pets: results.docs, pagesCount: results.pages, currentPage: page });
+      // Otherwise we do what we did before
+      } else {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      }
     });
   });
-  
 }
 // with this we are now limiting how many results we are getting on each page
 // with this next version we are able to access each page with 3 results per page
